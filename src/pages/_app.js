76954e7b40a3b8fe2802709afba90c2109ea01ea
Daniel <gleidson10daniel@hotmail.com>
2021-04-12 16:19:44 -0300

    Change overall layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,21 +3,25 @@ import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="mx-auto w-9/12 my-10">
+      <header>
+        <h1 className="text-6xl font-bold text-center">Daniel Silva</h1>
+        <nav className="my-5 ">
+          <ul className="flex flex-row justify-center space-x-4">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
       <Component {...pageProps} />
     </div>
   );
